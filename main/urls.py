f1f35987314dc5b87e2f34442da36aa3cf4fabbd
vivapyk <git hub vivapyk@gmail.com>
2019-02-07 20:50:12 +0900

    사소한 디버깅

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/', views.main_post, name='post'),
     path('post/<int:post_pk>', views.main_detail, name='detail'),
     path('create/', main_create, name='create'),
-    path('post/<int:post_pk>)/comment/new/', views.comment_new, name='comment_new'),
-    path('post/(<int:post_pk>)/comment/edit/', views.comment_edit, name='comment_edit'),
-    path('post/<int:post_pk>)/comment/delete/', views.comment_delete, name='comment_delete'),
+    path('post/<int:post_pk>/comment/new/', views.comment_new, name='comment_new'),
+    path('post/<int:post_pk>/comment/edit/', views.comment_edit, name='comment_edit'),
+    path('post/<int:post_pk>/comment/delete/', views.comment_delete, name='comment_delete'),
 ]
