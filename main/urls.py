e615d74a89f619add37759d4f2dc589a2b7cd81b
vivapyk <git hub vivapyk@gmail.com>
2019-02-07 18:33:49 +0900

    가장 최신

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main.views import main_detail, main_create, main_page, main_post
 urlpatterns = [
     path('', main_page, name='main'),
     path('post/', main_post, name='post'),
-    path('<int:pk>', main_detail, name='detail'),
+    path('post/<int:pk>', main_detail, name='detail'),
     path('create/', main_create, name='create'),
 ]
