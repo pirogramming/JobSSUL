3ab2a7e85a563f5f55a5ad99891766f85efd12ed
rrabit42 <gegiraffe@naver.com>
2019-02-07 22:28:39 +0900

    로그인/로그아웃 기본툴 완료(꾸미자)

--- jobssul/urls.py
+++ jobssul/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls', namespace='main')),
+    path('accounts/', include('accounts.urls')),
 ]
