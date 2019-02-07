02aa393be1f6ca5b6f2a78709104f9f9f1dd0cd5
vivapyk <git hub vivapyk@gmail.com>
2019-02-07 16:56:02 +0900

    draft1

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from main.views import main_post, main_detail
+from main.views import main_detail, main_create, main_page, main_post
 
 urlpatterns = [
-    path('', main_post, name='main'),
-    path('<int:pk>', main_detail, name='detail')
+    path('', main_page, name='main'),
+    path('post/', main_post, name='post'),
+    path('<int:pk>', main_detail, name='detail'),
+    path('create/', main_create, name='create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from main.form import PostForm
 from main.models import Post
 
 # Create your views here.
 
 
-def main_post(request):
+def main_page(request):
     post = Post.objects.all()
     data = {
         'post': post
@@ -13,10 +14,41 @@ def main_post(request):
     return render(request, 'main/main.html', data)
 
 
+def main_post(request):
+    post = Post.objects.all()
+    data = {
+        'post':post
+    }
+    return render(request, 'main/post.html', data)
+
 def main_detail(request, pk):
     post = Post.objects.get(pk=pk)
     data = {
         'post': post,
     }
     return render(request, 'main/detail.html', data)
+
+
+def main_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        # title = request.POST.title
+        # content = request.POST.content
+        # payment = request.POST.payment
+        # workplace = request.POST.payment
+        # recommend = request.POST.recommend
+        # work_type = request.POST.work_type
+
+        if form.is_valid():
+            post = Post.objects.create(title = form.cleaned_data['title'], content=form.cleaned_data['content'],
+                                       payment = form.cleaned_data['payment'], workplace = form.cleaned_data['recommend'],
+                                       work_type = form.cleaned_data['work_type'], recommend = form.cleaned_data['recommend'])
+            return redirect('main')
+    else:
+        form = PostForm()
+    return render(request, 'main/create.html', {
+        'form': form,
+    })
+
+
 
