356760fe3e93fd3b37430d07858f9b90e250997b
vivapyk <git hub vivapyk@gmail.com>
2019-02-07 22:43:06 +0900

    회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(redirect(settings.LOGIN_URL))
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
+
