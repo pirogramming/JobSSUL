287e71b105b016dbf35e30a9b45a6eaab07a8933
rrabit42 <gegiraffe@naver.com>
2019-02-10 16:29:31 +0900

    url 살짝 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ app_name ='accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     # path('login/', views.login, name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html', authentication_form = LoginForm), name='login'),
+    path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
 ]
 
